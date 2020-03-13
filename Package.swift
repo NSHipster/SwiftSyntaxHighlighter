166f401d221ef5e0653c137ea8fd6b36256bb2ae
Mattt <mattt@me.com>
2020-03-13 08:20:09 -0700

    Add package products

--- Package.swift
+++ Package.swift
@@ -5,6 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftSyntaxHighlighter",
+    products: [
+        .library(
+            name: "SwiftSyntaxHighlighter",
+            targets: ["SwiftSyntaxHighlighter"]),
+        .executable(name: "swift-highlight",
+                    targets: ["swift-highlight"])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", .revision("swift-5.2-DEVELOPMENT-SNAPSHOT-2020-03-09-a")),
         .package(url: "https://github.com/IBM-Swift/swift-html-entities.git", .upToNextMajor(from: "3.0.0"))
