fe39b4ec07e1e37872adf4b506d223ab27cf8cea
Mattt <mattt@me.com>
2020-03-16 11:55:16 -0700

    Treat stringInterpolationAnchor as String token

--- Sources/TokenizationSchemes/Xcode/Xcode.swift
+++ Sources/TokenizationSchemes/Xcode/Xcode.swift
@@ -38,7 +38,8 @@ public enum Xcode: TokenizationScheme {
         case .integerLiteral,
              .floatingLiteral:
             token = Token(tokenSyntax.text, kind: NumberLiteral.self)
-        case .stringLiteral:
+        case .stringLiteral,
+             .stringInterpolationAnchor:
             token = Token(tokenSyntax.text, kind: StringLiteral.self)
         case .poundDirectiveKeyword,
              .buildConfigId:
