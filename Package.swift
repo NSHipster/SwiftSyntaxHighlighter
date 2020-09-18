a7401ed05abfe27b3b2f3cc64860cbb15ed6c74a
Max Desiatov <max@desiatov.com>
2020-09-18 23:00:40 +0100

    Test on Linux with Swift 5.3, bump SwiftSyntax

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
                     targets: ["swift-highlight"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", .revision("0.50200.0")),
+        .package(url: "https://github.com/apple/swift-syntax.git", .revision("0.50300.0")),
         .package(url: "https://github.com/IBM-Swift/swift-html-entities.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.0.4")),
     ],
